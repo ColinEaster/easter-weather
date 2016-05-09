20988f979a99030105c1ccde28f942dd21bd3132
Colin Easter <Colin@colins-macbook-pro.local>
2016-05-09 11:03:27 -0400

    Initial Commit

---
+++ Easter Weather/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Easter Weather
+//
+//  Created by Colin Easter on 5/9/16.
+//  Copyright © 2016 Colin Easter. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Easter WeatherTests/Easter_WeatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  Easter_WeatherTests.swift
+//  Easter WeatherTests
+//
+//  Created by Colin Easter on 5/9/16.
+//  Copyright © 2016 Colin Easter. All rights reserved.
+//
+
+import XCTest
+@testable import Easter_Weather
+
+class Easter_WeatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
