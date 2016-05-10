71ac616958eea36d33ea4574b0796f7dd94f9e1b
Colin Easter <Colin@colins-macbook-pro.local>
2016-05-10 14:26:44 -0400

    Add WeatherData structure.

---
+++ Easter Weather/WeatherData.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherData.swift
+//  Easter Weather
+//
+//  Created by Colin Easter on 5/10/16.
+//  Copyright © 2016 Colin Easter. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData{
+    
+    var zipCode:Int
+    var latitude:Double?
+    var longitude:Double?
+    var placeName:String?
+    var currentTemperature:Double?
+    
+    init(zipCode: Int){
+        self.zipCode = zipCode
+    }
+}
