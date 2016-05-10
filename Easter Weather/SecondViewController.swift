30d9c6af40634b806a5d1c1a52c24014a9ad2a0f
Colin Easter <Colin@colins-macbook-pro.local>
2016-05-10 18:28:30 -0400

    Add pins to the map based on location and containing the current temperature.

--- Easter Weather/SecondViewController.swift
+++ Easter Weather/SecondViewController.swift
@@ -9,12 +9,61 @@
 import UIKit
 import MapKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, MKMapViewDelegate {
+    
+    let sharedData = SharedData.sharedInstance
     
     @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewWillAppear(animated: Bool) {
+        sharedData.data = SharedData.sharedInstance.data
+        print("in second view controller")
+        print(sharedData.data.count)
+        
+        print("loading second view controller")
+        print(sharedData.data.count)
+        
+        mapView.delegate = self
+        
+        var annotations = [MKPointAnnotation]()
+        print(sharedData.data.count)
+        for datum in sharedData.data {
+            
+            // skip this entry if it doesn't have a lat/long
+            guard let lat = datum.latitude, long = datum.longitude else{
+                continue
+            }
+            
+            print("past guard")
+            print(lat)
+            print(long)
+            
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            let zipCode = String(datum.zipCode)
+            
+            var temperature:String = "No temperature data found."
+            
+            if let temp = datum.currentTemperature{
+                temperature = String(format: "%.1f", temp) + "°"
+            }
+            
+            // Here we create the annotation and set its coordiate, title, and subtitle properties
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = zipCode
+            annotation.subtitle = temperature
+            
+            // Finally we place the annotation in an array of annotations.
+            annotations.append(annotation)
+        }
+        
+        // When the array is complete, we add the annotations to the map.
+        self.mapView.addAnnotations(annotations)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +71,6 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
