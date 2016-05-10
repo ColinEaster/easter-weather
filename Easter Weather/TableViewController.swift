9efef89df91c406202b3d9e05cffb6528f9bc326
Colin Easter <Colin@colins-macbook-pro.local>
2016-05-09 20:47:41 -0400

    Add a UITextField that accepts a zip code.

--- Easter Weather/TableViewController.swift
+++ Easter Weather/TableViewController.swift
@@ -11,16 +11,23 @@ import UIKit
 class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var zipcode = [17042, 18042]
+    let zipCodeDelegate = ZipCodeTextFieldDelegate()
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    
+    
     
     override func viewWillAppear(animated: Bool) {
         tableView.delegate = self
         tableView.dataSource = self
+        self.zipCodeTextField.delegate = self.zipCodeDelegate
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.addDoneButtonOnKeyboard()
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,6 +56,30 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         return cell
     }
+    // taken from http://iostechsolutions.blogspot.com/2014/11/swift-add-uitoolbar-or-done-button-on.html because the number pad doesn't have a return key... (and a touch gesture outside the keypad might conflict with selecting a cell)
+    func addDoneButtonOnKeyboard()
+    {
+        
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRectMake(0, 0, self.tableView.bounds.size.width, 50))
+        doneToolbar.barStyle = .Default
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Done, target: self, action: #selector(TableViewController.doneButtonAction))
+        
+        var items = [UIBarButtonItem]()
+        items.append(flexSpace)
+        items.append(done)
+        doneToolbar.items = items
+        
+        doneToolbar.sizeToFit()
+        
+        self.zipCodeTextField.inputAccessoryView = doneToolbar
+        self.zipCodeTextField.inputAccessoryView = doneToolbar
+        
+    }
+    func doneButtonAction()
+    {
+        self.zipCodeTextField.resignFirstResponder()
 
+    }
 }
 
